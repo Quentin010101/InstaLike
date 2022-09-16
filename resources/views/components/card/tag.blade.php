edf50e7c6b13112d5610dc72d16d9dd9a8d63fe6
unknown <q_cozic@hotmail.fr>
2022-09-16 19:24:07 +0200

    message

--- resources/views/components/card/tag.blade.php
+++ resources/views/components/card/tag.blade.php
@@ -1,3 +1,3 @@
-<div class="px-2 py-1 bg-red-200 hover:px-3 hover:bg-red-400 duration-300 text-sm text-white font-semibold w-fit rounded-full">
+<div class="px-2 py-1 bg-red-200 dark:bg-red-400 dark:hover:bg-red-200 hover:px-3 hover:bg-red-400 duration-300 text-sm text-white font-semibold w-fit rounded-full">
     {{ $slot }}
 </div>
--- resources/views/tag/tagShow.blade.php
+++ resources/views/tag/tagShow.blade.php
@@ -1,21 +1,67 @@
 <x-layout>
     <x-navigation></x-navigation>
     <main class="pt-16">
-        <div class="grid grid-cols-2 mx-auto">
-            <div>
-                @foreach ($images as $image)
-                @if($loop->even)
-                    <x-card.card-tag :image="$image" :id="$id"></x-card.card-tag>
-                    @endif
-                @endforeach
-            </div>
-            <div>
-                @foreach ($images as $image)
-                @if($loop->odd)
-                <x-card.card-tag :image="$image" :id="$id"></x-card.card-tag>
-                @endif
-                @endforeach
+        <div x-data>
+            <div id="container" @resize.window="resize()" class="flex">
+                <div id="colone1">
+                    @foreach ($images as $image)
+                        <div id="{{ $loop->iteration }}">
+                            <x-card.card-tag :image="$image" :id="$id"></x-card.card-tag>
+                        </div>
+                    @endforeach
+                </div>
             </div>
         </div>
+        <script>
+            const container = document.getElementById('container')
+            const colone1 = document.getElementById('colone1')
+            const colone2 = document.getElementById('colone2')
+            let status = {
+                'xl': '',
+                'sm': ''
+            }
+
+
+            if(window.innerWidth > 1000){
+                status.xl = true
+                status.sm = false
+            }else{
+                status.xl = false
+                status.sm = true
+            }
+
+            function resize()
+            {
+                let width = window.innerWidth;
+                if(width > 1000 && !status.xl){
+
+                    const newDiv = document.createElement('div')
+                    newDiv.setAttribute('id', 'colone2')
+                    container.appendChild(newDiv)
+
+                    let children = colone1.children
+                    let array = Array.from(children)
+                    let length = array.length
+
+                    for(let index = 0; index < length; index = index + 2){
+                        colone1.removeChild(array[index])
+                        newDiv.appendChild(array[index])
+                    }
+
+                    status.xl = true
+                    status.sm = false
+                }
+                if(width < 1000 && !status.sm){
+
+                    if (document.contains(document.getElementById("colone2"))) {
+                        document.getElementById('colone2').remove()
+                    }
+
+                    status.xl = false
+                    status.sm = true
+                }
+            }
+
+        </script>
     </main>
 </x-layout>
