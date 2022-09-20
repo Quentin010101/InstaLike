e6159574dd3df30f63ad2b1e1d92129d081cac72
unknown <q_cozic@hotmail.fr>
2022-09-20 14:43:57 +0200

    r

--- resources/views/livewire/profile-avatar.blade.php
+++ resources/views/livewire/profile-avatar.blade.php
@@ -5,14 +5,15 @@
             <div class="bg-gradient-to-r from-red-400 to-purple-500 rounded-full w-56 h-56 flex justify-center items-center">
                 <div class="relative rounded-full h-52 w-52 overflow-hidden bg-white dark:bg-slate-600">
                     @if ($avatar)
-                        <img src="{{ $avatar->temporaryUrl() }}">
-                    @endif
+                        <img class="object-fill h-52 w-52" src="{{ $avatar->temporaryUrl() }}">
+                    @else
                     <div class="absolute top-0 left-0 rounded-full h-52 w-52 overflow-hidden">
                         <label
                             class="block h-full w-full cursor-pointer flex justify-center items-center text-lg font-semibold text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 hover:text-gray-700 duration-200"
-                            for="avatar_input">Clik here</label>
+                            for="avatar_input">Click here</label>
                         <input class=" hidden" id="avatar_input" type="file" wire:model="avatar">
                     </div>
+                    @endif
                 </div>
             </div>
         </div>
@@ -22,6 +23,7 @@
             @enderror
         </div>
         <div class="my-5 w-full flex justify-center">
+            @if ($avatar)
             <div wire:loading.remove>
                 <x-buttons.validation>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -31,6 +33,7 @@
                     <p>Click to upload</p>
                 </x-buttons.validation>
             </div>
+            @endif
             <x-loader.loader target="avatar_upload"></x-loader.loader>
             <x-loader.loader target="avatar"></x-loader.loader>
         </div>
